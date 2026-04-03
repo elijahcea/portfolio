50677a4c93fc521dcc9ec0babc2a1b11118ad7c0
elijahcea <elijah.cea01@gmail.com>
2026-04-03 11:01:45 -0400

    create footer

---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+import {
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
+import Link from "next/link";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="flex flex-col items-center gap-4 mt-auto text-xs py-12 w-full">
+      <div className="flex items-center gap-4">
+        <a href="" target="_blank">
+          <IconBrandGithub />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandLinkedin />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandInstagram />
+        </a>
+      </div>
+
+      <div>
+        © {year}{" "}
+        <Link href="/">
+          <span className="inline">elijahcea.com</span>{" "}
+        </Link>
+      </div>
+    </footer>
+  );
+}
