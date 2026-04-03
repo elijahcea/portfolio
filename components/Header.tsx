7905e454e97dac8f45f1cc48f48b41517fa6ebcb
elijahcea <elijah.cea01@gmail.com>
2026-04-03 11:02:09 -0400

    add padding and icon to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import NavMenu from "./NavMenu";
+import { IconRocket } from "@tabler/icons-react";
 
 export default function NavBar() {
-    return (
-        <header className="p-4 flex items-center justify-between">
-            <Link href={'/'}>Elijah Cea</Link>
-            <nav>
-                <NavMenu />
-            </nav>
-        </header>
-    )
+  return (
+    <header className="p-6 flex items-center justify-between sticky top-0 z-10 backdrop-blur-sm">
+      <Link
+        href={"/"}
+        className="font-semibold italic inline-flex gap-1 items-center"
+      >
+        <IconRocket color="lab(64.4125 63.0291 19.2068)" />
+        elijahcea
+      </Link>
+      <nav>
+        <NavMenu />
+      </nav>
+    </header>
+  );
 }
