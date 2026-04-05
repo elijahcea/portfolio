9344631996e10980f8adf0a08a6b330a82a78876
elijahcea <elijah.cea01@gmail.com>
2026-04-04 22:49:43 -0400

    add nav for larger screen sizes

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { IconRocket } from "@tabler/icons-react";
 
 export default function NavBar() {
   return (
-    <header className="p-6 flex items-center justify-between sticky top-0 z-10 backdrop-blur-sm">
+    <header className="py-2 px-4 h-24 flex items-center justify-between sticky top-0 z-10 backdrop-blur-sm">
       <Link
         href={"/"}
         className="font-semibold italic inline-flex gap-1 items-center"
@@ -12,9 +12,7 @@ export default function NavBar() {
         <IconRocket color="lab(64.4125 63.0291 19.2068)" />
         elijahcea
       </Link>
-      <nav>
-        <NavMenu />
-      </nav>
+      <NavMenu />
     </header>
   );
 }
--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -21,75 +21,99 @@ export default function NavMenu() {
 
   return (
     <>
-      <button
-        onClick={() => setIsOpen(true)}
-        className="relative cursor-pointer transition-opacity ease-in-out duration-250 text-foreground hover:opacity-60 align-middle"
-      >
-        <Bars3Icon className="size-7" />
-      </button>
-      <Dialog open={isOpen} onClose={setIsOpen} className="relative z-15">
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-gray-500/75 backdrop-blur-xs transition-opacity duration-250 ease-in-out data-closed:opacity-0"
-        />
+      <nav className="md:hidden">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="p-2 relative cursor-pointer transition-opacity ease-in-out duration-250 text-foreground hover:opacity-60 align-middle"
+        >
+          <Bars3Icon className="size-8" />
+        </button>
+        <Dialog open={isOpen} onClose={setIsOpen} className="relative z-15">
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-gray-500/75 backdrop-blur-xs transition-opacity duration-250 ease-in-out data-closed:opacity-0"
+          />
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
-              <DialogPanel
-                transition
-                className="pointer-events-auto w-screen max-w-64 transform transition duration-250 ease-in-out data-closed:translate-x-full sm:duration-350"
-              >
-                <div className="flex h-full flex-col overflow-y-auto bg-background shadow-xl">
-                  <div className="flex-1 overflow-y-auto">
-                    <div className="flex items-start justify-between p-4 border-b border-foreground/10">
-                      <DialogTitle className="text-lg font-semibold text-rose-400">
-                        Navigation
-                      </DialogTitle>
-                      <div className="ml-3 flex h-7 items-center">
-                        <button
-                          type="button"
-                          onClick={() => setIsOpen(false)}
-                          className="cursor-pointer relative -m-2 p-2 text-gray-400 hover:text-gray-500"
-                        >
-                          <XMarkIcon aria-hidden="true" className="size-6" />
-                        </button>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+                <DialogPanel
+                  transition
+                  className="pointer-events-auto w-screen max-w-64 transform transition duration-250 ease-in-out data-closed:translate-x-full sm:duration-350"
+                >
+                  <div className="flex h-full flex-col overflow-y-auto bg-background shadow-xl">
+                    <div className="flex-1 overflow-y-auto">
+                      <div className="flex items-start justify-between p-4 border-b border-foreground/10">
+                        <DialogTitle className="text-lg font-semibold text-rose-400">
+                          Navigation
+                        </DialogTitle>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            onClick={() => setIsOpen(false)}
+                            className="cursor-pointer relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                          >
+                            <XMarkIcon aria-hidden="true" className="size-6" />
+                          </button>
+                        </div>
                       </div>
-                    </div>
 
-                    <nav className="font-semibold p-4 border-b border-foreground/10">
-                      <ul className="flex flex-col gap-4">
-                        {links.map((i) => (
-                          <Link key={i.title} href={i.href}>
+                      <nav className="font-semibold p-4 border-b border-foreground/10">
+                        <ul className="flex flex-col gap-4">
+                          {links.map((i) => (
+                            <Link key={i.title} href={i.href}>
+                              <li
+                                onClick={() => setIsOpen(false)}
+                                className="py-2 px-3 rounded transition-colors hover:bg-foreground/10 duration-250"
+                              >
+                                {i.title}
+                              </li>
+                            </Link>
+                          ))}
+                          <a
+                            href="resume.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
                             <li
                               onClick={() => setIsOpen(false)}
-                              className="p-2 rounded transition-colors hover:bg-foreground/10 duration-250"
+                              className="py-2 px-3 rounded transition-colors hover:bg-foreground/10 duration-250"
                             >
-                              {i.title}
+                              Resume
                             </li>
-                          </Link>
-                        ))}
-                        <a
-                          href="resume.pdf"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <li
-                            onClick={() => setIsOpen(false)}
-                            className="p-2 rounded transition-colors hover:bg-foreground/10 duration-250"
-                          >
-                            Resume
-                          </li>
-                        </a>
-                      </ul>
-                    </nav>
+                          </a>
+                        </ul>
+                      </nav>
+                    </div>
                   </div>
-                </div>
-              </DialogPanel>
+                </DialogPanel>
+              </div>
             </div>
           </div>
-        </div>
-      </Dialog>
+        </Dialog>
+      </nav>
+      <nav className="max-md:hidden">
+        <ul className="font-semibold flex gap-4">
+          {links.map((i) => (
+            <Link key={i.title} href={i.href}>
+              <li
+                onClick={() => setIsOpen(false)}
+                className="py-2 px-3 rounded transition-colors hover:bg-foreground/10 duration-250"
+              >
+                {i.title}
+              </li>
+            </Link>
+          ))}
+          <a href="resume.pdf" target="_blank" rel="noopener noreferrer">
+            <li
+              onClick={() => setIsOpen(false)}
+              className="py-2 px-3 rounded transition-colors hover:bg-foreground/10 duration-250"
+            >
+              Resume
+            </li>
+          </a>
+        </ul>
+      </nav>
     </>
   );
 }
